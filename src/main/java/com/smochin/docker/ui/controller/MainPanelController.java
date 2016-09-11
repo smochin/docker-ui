1f3b5059147d8d3fc35a9e0df16c93bf55cf7481
root <root@debian.usto.re>
2016-09-11 15:12:22 -0300

    Initial commit

---
+++ src/main/java/com/smochin/docker/ui/controller/MainPanelController.java
@@ -0,0 +1,13 @@
+package com.smochin.docker.ui.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class MainPanelController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }    
+    
+}
